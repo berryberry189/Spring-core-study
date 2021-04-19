c8e5e3b2f77a584857cccd0dd9dca4673a075835
berryberry189 <berryberry189@naver.com>
2021-04-19 09:41:21 +0900

    컴포넌트 스캔 -탐색 위치, 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,8 +7,20 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan (
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
-)     // excludeFilters 로 설정하지 않은 @Component 가 붙은 설정 정보를 자동으로 등록해줌
-      // @Configuration 인터페이스를 확인하면 @Component 가 붙은것을 확인할 수 있음
+)
+/**
+ * @ComponentScan -
+        excludeFilters 로 설정하지 않은 @Component 가 붙은 설정 정보를 자동으로 등록해줌
+        @Configuration 인터페이스를 확인하면 @Component 가 붙은것을 확인할 수 있음
+        basePackages :  탐색할 패키지의 시작 위치를 지정, 이 패키지를 포함해서 하위 패키지를 모두 탐색
+        basePackageClasses : 지정한 클래스의 패키지를 탐색 시작 위치로 지정
+        만약 지정하지 않으면 @ComponentScan 이 붙은 설정 정보 클래스의 패키지가 시작 위치가 된다.
+
+    < 권장 방법 >
+    패키지 위치를 지정하지 않고, 설정 정보 클래스의 위치를 프로젝트 최상단에 두는 것
+    - 최근 스프링 부트도 이 방법을 기본으로 제공한다
+ */
+
 public class AutoAppConfig {
 
 }
